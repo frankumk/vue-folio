747c18f7271e6278fc5af891e3563ffa68c972f4
Kayla <frankumk@gmail.com>
2021-05-19 16:49:42 -0700

    Added header, footerr

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,52 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <v-toolbar color="#83B4B3" dark>
+    <!-- <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon> -->
+        <v-tabs v-model="tab"> 
+        <v-tab left to="/">Kayla Frankum</v-tab>
+        <v-spacer></v-spacer>
+        <v-tab right grow to="/resume">Resume</v-tab>
+      </v-tabs>
+    
+      </v-toolbar>
     </div>
-    <router-view/>
+
+    <div id="main">
+      <router-view/>
+    </div>
+
+    <v-footer color="#83B4B3">
+      <v-col
+        class="text-center"
+      >
+      <v-btn
+        v-for="icon in icons"
+        :key="icon"
+        class="mx-4"
+        icon
+      >
+      <v-icon color="black">{{ icon }}</v-icon>
+      </v-btn>
+      </v-col>
+    </v-footer>
   </div>
 </template>
+<script>
+
+  export default{
+    data: () => ({
+      tab: null,
+      icons: [
+        'mdi-email',
+        'mdi-github',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ]
+    })
+  }
+
+</script>
 
 <style>
 #app {
