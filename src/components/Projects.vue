ca6ee30d4382c9d6c4a5b72c59db9c75a137bc26
Kayla <frankumk@gmail.com>
2021-05-25 21:09:33 -0700

    Before mq

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -25,7 +25,7 @@ import ProjectCard from './ProjectCard'
       projects: [
           { id: 0, name: 'Doombuster', img: require("../assets/Doombuster.png"), description: 'Add/upvote songs & answer trivia in shared room to select next song.', link: 'https://youtu.be/GUi3Z5kBZHw' },
           { id: 1, name: 'Mummy Mario', img: require("../assets/Mummymario.png"), description: 'One level of Mario inspired game. Dodge monsters & collect items.', link: 'https://youtu.be/B7Mrv-OFl8A' },
-          { id: 2, name: 'Bookstore', img: require("../assets/Bookstore.png"), description: 'Ecommerce site with admin, guest & customer checkout', link: 'https://jaykaytay-books.herokuapp.com/allBooks' },
+          { id: 2, name: 'Bookstore', img: require("../assets/Bookstore.png"), description: 'Ecommerce site with admin tools, guest & customer checkout', link: 'https://jaykaytay-books.herokuapp.com/allBooks' },
           { id: 6, name: 'Meter', img: require("../assets/Meter.png"), description: "Leave a review for your favorite or worst experiences.", link: 'https://github.com/frankumk/Leave_Review'},
           { id: 3, name: 'Birthday Tracker', img: require("../assets/Birthdays.png"), description: "Get reminders for friends' birthdays.", link: 'https://github.com/frankumk/dealers_choice_react' } ,
           { id: 4, name: 'To Do List', img: require("../assets/ToDoList.png"), description: 'Multi-list maker', link: 'https://github.com/frankumk/to_do' },
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
   <h1 id="skills-title">SKILLS</h1>
-  <p>Portfolio Built Using Vue and Vuetify</p>
+  <p>Portfolio Built Using Vue.js and Vuetify</p>
   <v-img src="../assets/WordCloud2.png" alt="Skill Wordcloud" />
 </div>
 </template>
